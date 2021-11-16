060ddd2083701a1567fa6f05504f818a5ae9b18b
Pary <81phary@gmail.com>
2021-11-16 17:49:17 +0700

    adding type writting effect and adding back to top and bottom button

---
+++ js/index.js
@@ -0,0 +1,35 @@
+let textDiv = document.querySelector(".text");
+let textDiv2 = document.querySelector(".text2");
+let text = "WELCOME TO THE NEW WORLD";
+let text2 = "OUR WORLD";
+let i = 0;
+let j = 0;
+var t;
+
+function type(){
+    if (i < text.length){
+        textDiv.innerHTML += text.charAt(i);
+        i++;
+        t=setTimeout(type, 200);
+    } else if (j < text2.length){
+        textDiv2.innerHTML += text2.charAt(j);
+        j++;
+        t=setTimeout(type, 200);
+    }
+    if (j===text2.length){
+        t=setTimeout(clear,4000);
+        
+    }
+    
+}
+
+function clear(){
+    i=0;
+    j=0;
+    clearTimeout(t);
+    textDiv.innerHTML = "";
+    textDiv2.innerHTML = "";
+    type();
+}
+
+window.addEventListener("load",type)
